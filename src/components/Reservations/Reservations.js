27e2659ebed58cf519df9822325980c93e85927e
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-21 12:05:54 -0600

    Pass down deleteReservation

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -2,8 +2,13 @@ import React from 'react'
 import Reservation from '../Reservation/Reservation'
 import './Reservations.css'
 
-const Reservations = ({reservationData}) => {
-    const reservations = reservationData.map(reservation => <Reservation key={reservation.id} reservationInfo={reservation}/>)
+const Reservations = ({reservationData, deleteReservation}) => {
+    const reservations = reservationData.map(reservation => 
+    <Reservation 
+        key={reservation.id} 
+        reservationInfo={reservation}
+        deleteReservation={deleteReservation}
+    />)
     return (
         <section className='reservation-container'>
             {reservations}
