b340a728da0eee7cee8c5fade2365abd013dd74a
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-21 09:37:13 -0600

    Import reservation component

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Reservation from '../Reservation/Reservation'
 
 const Reservations = ({reservationData}) => {
     const reservations = reservationData.map(reservation => <Reservation key={reservation.id} reservationInfo={reservation}/>)
