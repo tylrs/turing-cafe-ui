302bf6a4352d98e4f4b4b4c6784171f507a02ff3
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-21 09:51:22 -0600

    Add basic styling for cards

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Reservation.css'
 
 const Reservation = ({reservationInfo}) => {
     const {name, date, time, number} = reservationInfo;
     return (
-        <article>
+        <article className='reservation-card'>
             <h2>{name}</h2>
             <p>{date}</p>
             <p>{time}</p>
