fe2dafda7396464d0d557e3713e84a782ff141ff
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-21 10:06:51 -0600

    Render form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Reservations from '../Reservations/Reservations'
+import ReservationForm from '../ReservationForm/ReservationForm'
 
 class App extends Component {
   constructor() {
@@ -31,7 +32,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <ReservationForm />
         </div>
         <div className='resy-container'>
           <Reservations reservationData={this.state.reservations}/>
