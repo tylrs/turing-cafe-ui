da1aa07669390974e5e4b7d4dde5d44e2cf0cdf8
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-21 09:44:44 -0600

    Add grid to outer component

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Reservation from '../Reservation/Reservation'
+import './Reservations.css'
 
 const Reservations = ({reservationData}) => {
     const reservations = reservationData.map(reservation => <Reservation key={reservation.id} reservationInfo={reservation}/>)
     return (
-        <section>
+        <section className='reservation-container'>
             {reservations}
         </section>
     )
