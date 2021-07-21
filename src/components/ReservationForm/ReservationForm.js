52d4aedc12fad1128e308ab2bfc6728cfc6f9cc9
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-21 10:02:52 -0600

    Add form JSX

--- src/components/ReservationForm/ReservationForm.js
+++ src/components/ReservationForm/ReservationForm.js
@@ -10,6 +10,51 @@ class ReservationForm extends component {
             number: 0
         }
     }
+
+    render() {
+        return (
+        <form>
+            <label>
+                <input 
+                    type='text'
+                    placeholder='name'
+                    name='name'
+                    value={this.state.name}
+                    onChange = {(event) => handleChange(event)}
+                />
+            </label>
+            <label>
+                <input 
+                    type='text'
+                    placeholder='Date (mm/dd)'
+                    name='date'
+                    value={this.state.date}
+                    onChange = {(event) => handleChange(event)}
+                />
+            </label>
+            <label>
+                <input 
+                    type='text'
+                    placeholder='Time'
+                    name='time'
+                    value={this.state.time}
+                    onChange = {(event) => handleChange(event)}
+                />
+            </label>
+            <label>
+                <input 
+                    type='number'
+                    placeholder='Number of Guests'
+                    name='number'
+                    value={this.state.number}
+                    onChange = {(event) => handleChange(event)}
+                />
+            </label>
+            <button>Make Reservation</button>
+        </form>
+
+        )
+    }
 }
 
 export default ReservationForm
