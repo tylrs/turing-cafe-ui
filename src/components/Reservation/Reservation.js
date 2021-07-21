6bc758fe4f5b30b6316910986a3b3a5bad0fd2a5
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-21 12:05:38 -0600

    Add deleteMethod

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Reservation.css'
 
-const Reservation = ({reservationInfo}) => {
-    const {name, date, time, number} = reservationInfo;
+const Reservation = ({reservationInfo, deleteReservation}) => {
+    const {id, name, date, time, number} = reservationInfo;
     return (
         <article className='reservation-card'>
             <h2>{name}</h2>
             <p>{date}</p>
             <p>{time}</p>
             <p>Number of guests: {number}</p>
-            <button>Cancel</button>
+            <button onClick={() => {deleteReservation(id)}}>Cancel</button>
         </article>
     )
 }
